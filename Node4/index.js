258db7184d82b22a59569b5d22e69b0466900c8f
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-23 01:13:01 -0600

    vidly

--- Node4/index.js
+++ Node4/index.js
@@ -8,7 +8,6 @@ const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
   { id: 3, name: "course3" },
-  { id: 4, name: "course4" },
 ];
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -17,14 +16,6 @@ app.get("/", (req, res) => {
 app.get("/api/courses", (req, res) => {
   res.send(courses);
 });
-
-// app.get("/api/courses/:id", (req, res) => {
-//   res.send(req.params.id);
-// });
-
-// app.get("/api/posts/:year/:month", (req, res) => {
-//   res.send(req.query);
-// });
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
