b3521d92dcc22e53c37207f6b05bf8aa2739618e
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-27 00:16:45 -0600

    node exercise

--- Node6/exercise.js
+++ Node6/exercise.js
@@ -1,14 +1,27 @@
-getCustomer(1, (customer) => {
+// getCustomer(1, (customer) => {
+//   console.log("Customer: ", customer);
+//   if (customer.isGold) {
+//     getTopMovies((movies) => {
+//       console.log("Top movies: ", movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log("Email sent...");
+//       });
+//     });
+//   }
+// });
+
+async function notifyCustomer() {
+  const customer = await getCustomer(1);
   console.log("Customer: ", customer);
   if (customer.isGold) {
-    getTopMovies((movies) => {
-      console.log("Top movies: ", movies);
-      sendEmail(customer.email, movies, () => {
-        console.log("Email sent...");
-      });
-    });
+    const movie = await getTopMovies();
+    console.log("Top movies: ", movies);
+    await sendEmail(customer.email, movies);
+    console.log("Email sent...");
   }
-});
+}
+notifyCustomer();
+
 function getCustomer(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
